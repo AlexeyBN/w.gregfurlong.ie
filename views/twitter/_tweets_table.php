ae34ad0d9883bfc9f5c801b6641bc2f500b039fb
Alex <alexey1221babkin@gmail.com>
2015-08-18 20:46:56 +0300

    date fix

--- views/twitter/_tweets_table.php
+++ views/twitter/_tweets_table.php
@@ -13,7 +13,7 @@
             <?php foreach ($tweets as $tweet): ?>
                 <tr>
                     <td><?php echo $tweet->id ?></td>
-                    <td><?php echo date('Y/d/m H:i:s', $tweet->date) ?></td>
+                    <td><?php echo date('Y/d/m h:i:s A', $tweet->date) ?></td>
                     <td><?php echo $tweet->is_posted? "Posted": "Not Yet" ?></td>
                     <td>
                         <button data-id="<?php echo $tweet->id ?>" type="button" class="btn btn-danger remove-tweet">Delete</span></button>
